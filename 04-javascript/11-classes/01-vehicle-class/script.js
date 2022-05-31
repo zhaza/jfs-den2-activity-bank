acc1cb73b36489016d8c0db6cc9e4a0b27573582
zhaza <Khail.ballin12@yahoo.com>
2022-05-31 15:27:16 -0700

    Add two new objects

--- 04-javascript/11-classes/01-vehicle-class/script.js
+++ 04-javascript/11-classes/01-vehicle-class/script.js
@@ -1,21 +1,27 @@
 // Create a class called Vehicle
 class Vehicle {
     // This class should have 4 properties that are general to all vehicles. Example: type, hasMotor, color, weight
-    constructor(manufacturer, color, type, hasMotor)
-    this._manufacturer = manufacturer;
-    this._color = color;
-    this._type = type;
-    this._hasMoter = hasMotor;
+    constructor(manufacturer, color, type, hasMotor){
+        this._manufacturer = manufacturer;
+        this._color = color;
+        this._type = type;
+        this._hasMoter = hasMotor;
+    }
     // The class should also have 2 methods, one method that checks if the vehicle has a motor, if it does it should return "vroom vroom"
-    sound() {
+    sound(){
         if(this._thisHasMoter) {
             return 'vroom vroom';
         }
-    }
+    };
     // The second method is up to you. Could be a method that returns the color and weight of the vehicle
-    changeColor() {
+    changeColor(){
         this._color = 'gold';
         return `This car got an upgrade, and the color is now ${this._color}`;
-    }
+    };
 }
 // Create 2 objects from your Vehicle class
+const firstPersonal = new Vehicle('Nissan', 'Blue', 'Truck', true);
+const firstBusiness = new Vehicle('Mercedes', 'Silver', 'sedan', true);
+
+console.log(firstPersonal._hasMoter);
+console.log(firstBusiness.changeColor());
