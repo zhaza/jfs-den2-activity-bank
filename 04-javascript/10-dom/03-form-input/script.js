84154fc0be40f5948926f8a74af4f17dcf3b4a7e
zhaza <Khail.ballin12@yahoo.com>
2022-06-01 04:28:29 -0700

    Solved JS 10-form challenge

--- 04-javascript/10-dom/03-form-input/script.js
+++ 04-javascript/10-dom/03-form-input/script.js
@@ -1,6 +1,41 @@
-// DOM Variables
+// // // DOM Variables
+// let animal = document.getElementById("animal");
+// let place = document.getElementById("place");
+// let movie = document.getElementById("movie");
+// let number = document.getElementById("number");
+// let flexCheckDefault = document.getElementById("flexCheckDefault");
+// let flexCheck = document.getElementById("flexCheck");
+// let submit = document.getElementById("submit");
 
+// // // Create a function that console logs each piece of data from the user on submit
+// // // Look up event.preventDefault() on why it is needed for form submission.
 
+// const gatherInfo = function (event) {
+//   event.preventDefault();
+//   console.log(animal.value);
+//   console.log(place.value);
+//   console.log(movie.value);
+//   console.log(number.value);
+//   console.log(flexCheckDefault.value);
+//   console.log(flexCheck.value);
+// };
 
-// Create a function that console logs each piece of data from the user on submit
-// Look up event.preventDefault() on why it is needed for form submission.
+// submit.addEventListener("click", gatherInfo);
+
+// gathering user info using the form object
+
+let myForm = document.forms["myForm"];
+
+myForm.onsubmit = function (event) {
+  event.preventDefault();
+  console.log(myForm.animal.value);
+  console.log(myForm.place.value);
+  console.log(myForm.movie.value);
+  console.log(myForm.number.options[myForm.number.selectedIndex].value);
+  console.log(myForm.flexCheckDefault.checked);
+  console.log(myForm.flexCheck.checked);
+  console.log(myForm.inputState.value);
+  console.log(myForm.customRange1.value);
+  console.log(myForm.flexRadioDefault1.checked);
+  console.log(myForm.flexRadioDefault2.checked);
+};
