866929edc47b4a9cf814e27f59c928f4beaec829
zhaza <Khail.ballin12@yahoo.com>
2022-06-02 03:19:25 -0700

    Solve js 12-02

--- 04-javascript/12-modules-and-node/02-node-loop/utils.js
+++ 04-javascript/12-modules-and-node/02-node-loop/utils.js
@@ -2,17 +2,11 @@
 // The parameter will be an array of strings
 // create a loop inside the function that prints each string to the terminal
 
-
-
-
-
-
-
-
-
-
-
-
-
+const print = (arr) => {
+  for (let str of arr) {
+    console.log(str);
+  }
+};
 
 // export your function
+module.exports = { print };
