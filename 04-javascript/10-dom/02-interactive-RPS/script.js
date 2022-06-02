3a38c4475b19465c1e57b4a967506e7d533e64bc
zhaza <Khail.ballin12@yahoo.com>
2022-06-02 10:59:35 -0700

    Resolve merge conflict

--- 04-javascript/10-dom/02-interactive-RPS/script.js
+++ 04-javascript/10-dom/02-interactive-RPS/script.js
@@ -43,35 +43,57 @@ const compObj = {
 const gameObj = {
   gameCounter: 0,
   playGame(event) {
+    function reset() {
+      if (userObj.wins > userObj.losses) {
+        displayResult.innerText = "Player Won";
+      } else if (userObj.wins < userObj.losses) {
+        displayResult.innerText = "Player Lost";
+      } else {
+        displayResult.innerText = "Tie!";
+      }
+      gameObj.gameCounter = 0;
+      userObj.wins = 0;
+      userObj.losses = 0;
+      userObj.ties = 0;
+      return (displayVs.innerText = "Wanna play more?");
+    }
     this.gameCounter++;
     let userChoice = event.target.innerText;
     let computerChoice = compObj.generateCompChoice();
     displayCompChoice.innerText = computerChoice;
     displayVs.innerText = `${userChoice}    vs.    ${computerChoice}`;
     displayGamesPlayed.innerText = this.gameCounter;
-    if ((userChoice === "Rock" && computerChoice === "Scissors") ||
-            (userChoice === "Scissors" && computerChoice === "Paper") || 
-            (userChoice === "Paper" && computerChoice === "Rock")) {
-              userObj.wins++;
-              // compObj.losses++;
-              // displayGamesWon.parentElement.style.backgroundColor = "green";
-              // displayGamesWon.parentElement.style.color = "white";
-              displayResult.innerHTML = `Your ${userChoice} won this match!`
-            } else if (userChoice === computerChoice) {
-              userObj.ties++;
-              // compObj.ties++;
-              // displayGamesTied.parentElement.style.backgroundColor = "yellow";
-              displayResult.innerHTML = `The game is a tie`
-            } else {
-              userObj.losses++;
-              // compObj.wins++;
-              // displayGamesLost.parentElement.style.backgroundColor = "red";
-              // displayGamesLost.parentElement.style.color = "white";
-              displayResult.innerHTML = (userChoice === "Scissors" ? `Your Scissors were defeated...` : `Your ${userChoice} was defeated...`)
-            }
-            
-          displayWins.innerHTML = userObj.wins;
-          displayLosses.innerHTML = userObj.losses;
-          displayTies.innerHTML = userObj.ties
+    if (this.gameCounter === 5) {
+      return reset();
+    }
+    if (
+      (userChoice === "Rock" && computerChoice === "Scissors") ||
+      (userChoice === "Scissors" && computerChoice === "Paper") ||
+      (userChoice === "Paper" && computerChoice === "Rock")
+    ) {
+      userObj.wins++;
+      // compObj.losses++;
+      // displayGamesWon.parentElement.style.backgroundColor = "green";
+      // displayGamesWon.parentElement.style.color = "white";
+      displayResult.innerHTML = `Your ${userChoice} won this match!`;
+    } else if (userChoice === computerChoice) {
+      userObj.ties++;
+      // compObj.ties++;
+      // displayGamesTied.parentElement.style.backgroundColor = "yellow";
+      displayResult.innerHTML = `The game is a tie`;
+    } else {
+      userObj.losses++;
+      // compObj.wins++;
+      // displayGamesLost.parentElement.style.backgroundColor = "red";
+      // displayGamesLost.parentElement.style.color = "white";
+      displayResult.innerHTML =
+        userChoice === "Scissors"
+          ? `Your Scissors were defeated...`
+          : `Your ${userChoice} was defeated...`;
+    }
+
+    displayWins.innerHTML = userObj.wins;
+    displayLosses.innerHTML = userObj.losses;
+    displayTies.innerHTML = userObj.ties;
   },
 };
