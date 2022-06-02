f77a639e772c001ef22a1fe59867be3c89e94284
zhaza <Khail.ballin12@yahoo.com>
2022-06-02 03:49:52 -0700

    Solve first js assesssment

--- 04-javascript/00-Supplemental/js-1-js-2-assessment/1-bball-score.js
+++ 04-javascript/00-Supplemental/js-1-js-2-assessment/1-bball-score.js
@@ -1,3 +1,15 @@
 function totalBasketballScore(freeThrows, midRange, threePointers) {
-    
+  let sum = freeThrows + midRange*2 + threePointers*3;
+  if (
+    typeof freeThrows === "number" &&
+    typeof midRange === "number" &&
+    typeof threePointers === "number"
+  ) {
+    console.log(sum);
+  } else {
+    console.log("All entries must be numbers");
+  }
 }
+
+totalBasketballScore(2, 3, 4);
+
