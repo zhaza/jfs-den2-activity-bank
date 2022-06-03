79e41867f8e8ed3cb5e6dba810b5709bbfa956d7
zhaza <Khail.ballin12@yahoo.com>
2022-06-03 14:31:57 -0700

    Solve js 14-02

---
+++ 04-javascript/14-requests/01-basic-fetch-get/script.js
@@ -0,0 +1,9 @@
+fetch("https://reqres.in/api/users")
+.then(response => {
+    console.log(response);
+    return response.json();
+})
+.then(users => {
+    console.log(users);
+    
+})
--- 04-javascript/14-requests/02-asyncAwait-fetch-get/script.js
+++ 04-javascript/14-requests/02-asyncAwait-fetch-get/script.js
@@ -1,7 +1,17 @@
-fetch("https://reqres.in/api/users")
-.then(response => {
-    console.log(response);
-    return response.json();
-}).then(users => {
+// fetch("https://reqres.in/api/users")
+// .then(response => {
+//     console.log(response);
+//     return response.json();
+// }).then(users => {
+//     console.log(users);
+// })
+
+const requestUsers = async () => {
+    let response = await fetch("https://reqres.in/api/users");
+    let usersJson = await response.json();
+    return usersJson;
+}
+
+requestUsers().then(users => {
     console.log(users);
 })
