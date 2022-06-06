08a8c6f9ede630df610ae09833be35ebb5efb73f
zhaza <Khail.ballin12@yahoo.com>
2022-06-06 15:31:39 -0700

    Semi-solve js 14-05

--- 04-javascript/14-requests/05-post-user/script.js
+++ 04-javascript/14-requests/05-post-user/script.js
@@ -1,1 +1,25 @@
+const put = async () => {
+    let newName = document.getElementById('name');
+    let newJob = document.getElementById('job');
+    let response = await fetch('https://reqres.in/api/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: newName,
+            job: newJob
+        })
+    })
+    if(!response.ok){
+        throw Error(`There is an error with status ${response.status}`)
+    }
+    let usersJson = response.json();
+    let h1 = document.createElement('h1');
+    h1.innerText = `${newName.value} ${newJob.value}`;
+    document.body.appendChild(h1);
+    return usersJson.data;
+}
 
+let button = document.querySelector('button');
+button.addEventListener('click', put);
