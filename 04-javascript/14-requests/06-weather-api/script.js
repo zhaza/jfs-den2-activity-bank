f534c6b923d33792c1be13ec75a09217c0cb46f9
zhaza <Khail.ballin12@yahoo.com>
2022-06-08 19:46:06 -0700

    Connect and Fetch weather API

--- 04-javascript/14-requests/06-weather-api/script.js
+++ 04-javascript/14-requests/06-weather-api/script.js
@@ -2,26 +2,33 @@
 
 const put = async () => {
   let city = document.getElementById("city").value;
-  let response = await fetch(
-    'http://api.weatherapi.com/v1/current.json?key=b879833c9dc3491f8b8222128220706&q=London&aqi=no');
+  let request = `http://api.weatherapi.com/v1/current.json?key=b879833c9dc3491f8b8222128220706&q=${city}&aqi=no`
+  let response = await fetch(request);
+  console.log(response);
     if(!response.ok){
         throw Error(`There is an error with status ${response.status}`)
     }
+  let weather = await response.json();
+  console.log(weather);
   // let curWeatherJson = response.json();
   // let h1 = document.createElement('h1');
   // h1.innerText = `${newName.value} ${newJob.value}`;
   // document.body.appendChild(h1);
-  return response;
+  return weather;
 };
 
-const show = response => {
-  let weather = document.getElementById("weather");
+put();
+
+const show = async () => {
+  let location = document.getElementById("weather");
   let div = document.createElement("div");
   let h1 = document.createElement("h1");
-  h1.innerHTML = response;
-  console.log(response)
+  let current = await put();
+  h1.innerHTML = current.location.name;
+  h1.innerHTML = current.location.name;
+  h1.innerHTML = current.location.name;
   div.appendChild(h1);
-  weather.appendChild(div);
+  location.appendChild(div);
 };
 
 let button = document.querySelector("button");
